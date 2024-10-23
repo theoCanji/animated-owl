c44c5ecaac7ae4547233098b37a6db150d384adb
Jack Notarangelo <jack.notarangelo@du.edu>
2024-10-22 23:57:19 -0600

    Worked on BCNode and ConnectionHandler class

--- ConnectionHandler.java
+++ ConnectionHandler.java
@@ -1,16 +1,40 @@
 package projectTwo;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
-public class ConnectionHandler implements Runnable{
+public class ConnectionHandler implements Runnable {
+
+    private final int PORT;
+    private  ArrayList<BCNode> connNodes;
+
+    public ConnectionHandler(int port) {
+        PORT = port;
+    }
+
     @Override
     public void run() {
         try {
-            ServerSocket ss = new ServerSocket(port);
-            for (int i = 0; i < connNodes.size(); i++) {
+            ServerSocket ss = new ServerSocket(PORT);
+            while(true) {
+                System.out.println("Waiting for a call");
+                Socket s = ss.accept(); // blocking
+                System.out.println("Accepted");
+                ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+                BCNode bcn = (BCNode)ois.readObject();
+                int connPort = bcn.getPort();
+                connNodes.add(bcn);
+                System.out.println("Connected to " + bcn + " on port " + connPort);
+                ReadHandler readHandler = new ReadHandler(connPort, connNodes.getLast());
+                Thread rh = new Thread(readHandler);
+                rh.start();
+
+                s.close();
                 // threads[i] = new Thread(new BCNodeThread(ss, connNodes.get(i)));
-                    System.out.println("Waiting for a call");
-                    // nodes[i] = ss.accept(); // blocking
-                    System.out.println("Accepted");
+                // System.out.println("Waiting for a call");
+                // nodes[i] = ss.accept(); // blocking
+                // System.out.println("Accepted");
             }
         }
         catch (Exception e) {
--- ReadHandler.java
+++ ReadHandler.java
@@ -1,9 +1,23 @@
 package projectTwo;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ReadHandler implements  Runnable {
+
+    private final int PORT;
+    private BCNode connNode;
+
+    public ReadHandler(int port, BCNode connNode) {
+        PORT = port;
+        this.connNode = connNode;
+    }
+    
     @Override
     public void run() {
-        // TODO Auto-generated method stub
+        
         
     }
+
+    
 }
